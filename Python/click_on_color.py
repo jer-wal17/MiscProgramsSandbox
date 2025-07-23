7cc401377130b64e660cd3eee8839e5ca7c49efb
JD Waldron <jeromew1714@gmail.com>
2025-07-23 18:57:42 -0500

    Create click_on_color.py
    A program that stops and starts a Steam download to keep the local transfer going

---
+++ Python/click_on_color.py
@@ -0,0 +1,23 @@
+from time import sleep
+from datetime import datetime
+import pyautogui as pag
+
+now = datetime.now()
+
+download_button_x = 1460
+download_button_y = 288
+
+transfer_bar_x = 1039 * 2
+transfer_bar_y = 346 * 2
+transfer_bar_color = (75, 157, 248)
+
+while True:
+    print(now.strftime('%I:%M %p'), end='\t')
+    if not pag.pixelMatchesColor(transfer_bar_x, transfer_bar_y, transfer_bar_color):
+        print("Toggling download twice")
+        pag.click(download_button_x, download_button_y)
+        sleep(1)
+        pag.click(download_button_x, download_button_y)
+    else:
+        print("Transfer still good")
+    sleep(70)

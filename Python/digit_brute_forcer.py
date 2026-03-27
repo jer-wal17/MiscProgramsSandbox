ae881e048496f0b94144a69684f674a9f5c88d77
JD Waldron <jeromew1714@gmail.com>
2026-03-26 19:53:37 -0500

    Implement known digits

--- Python/digit_brute_forcer.py
+++ Python/digit_brute_forcer.py
@@ -1,19 +1,26 @@
+import re
+
 numDigits: int = int(input("Enter number of digits in code: "))
 
 numTotalPossibleSolutions: int = 10**numDigits
 
-print("Total possible solutions:", numTotalPossibleSolutions)
-
-USER_PROMPT_MESSAGE: str = "Enter a guess (Q/q to quit): "
+USER_PROMPT_MESSAGE: str = "Enter a guess (D/d to enter known digit(s), Q/q to quit): "
 
 
-def getUserInput() -> str:
-    return str(input(USER_PROMPT_MESSAGE))
+def getUserInput(prompt: str = USER_PROMPT_MESSAGE) -> str:
+    print("Remaining possible solutions:", len(allPossibleSolutions))
+    print("Known digits:", knownCombo)
+    return str(input(prompt))
+
+
+def continueThroughLoop(infomessage: str):
+    print(infomessage)
+    return getUserInput()
 
 
 def generateAllPossibleSolutions() -> list[str]:
     possSols: list[str] = []
-    for i in range(1, numTotalPossibleSolutions + 1):
+    for i in range(numTotalPossibleSolutions):
         possSols.append(str(i).zfill(numDigits))
 
     return possSols
@@ -25,20 +32,35 @@ def printFirstXPossibleSolutions(x: int) -> None:
     print()
 
 
+def keepKnownDigitCombos() -> tuple[list[str], str]:
+    knownDigts: str = str(
+        input("Enter known digits with 'x' in place of unknowns: "))
+    knownCombo = knownDigts.lower().replace("x", "_")
+    regexString: str = knownDigts.lower().replace("x", ".")
+    regex = re.compile(regexString)
+    return list(filter(regex.match, allPossibleSolutions)), knownCombo
+
+
 allPossibleSolutions: list[str] = generateAllPossibleSolutions()
+
+knownCombo: str = "___"
 
 userInput: str = getUserInput()
 while userInput.lower() != "Q".lower():
-    if len(userInput) != numDigits:
-        print("Invalid number of digits.")
+
+    if userInput.lower() == "D".lower():
+        allPossibleSolutions, knownCombo = keepKnownDigitCombos()
         userInput = getUserInput()
+        continue
+
+    if len(userInput) != numDigits:
+        userInput = continueThroughLoop("Invalid number of digits.")
         continue
 
     paddedUserInput: str = userInput.zfill(numDigits)
 
     if paddedUserInput not in allPossibleSolutions:
-        print("Already guessed.")
-        userInput = getUserInput()
+        userInput = continueThroughLoop("Already guessed.")
         continue
 
     allPossibleSolutions.remove(paddedUserInput)

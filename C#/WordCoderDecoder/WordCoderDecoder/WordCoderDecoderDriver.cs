06b25f4e67b12a39de4af904ec0af91a7f4ceaab
JD Waldron <jeromew1714@gmail.com>
2026-05-28 23:17:21 -0500

    Add letters to numbers dictionary

--- C#/WordCoderDecoder/WordCoderDecoder/WordCoderDecoderDriver.cs
+++ C#/WordCoderDecoder/WordCoderDecoder/WordCoderDecoderDriver.cs
@@ -4,6 +4,14 @@
     {
         public Dictionary<char, string> LetterBank { get; set; } = [];
 
+        public static Dictionary<char, int> LettersToNumbers { get; } = new() {
+                {'A', 1}, {'B', 2}, {'C', 3}, {'D', 4}, {'E', 5},
+                {'F', 6}, {'G', 7}, {'H', 8}, {'I', 9}, {'J', 10},
+                {'K', 11}, {'L', 12}, {'M', 13}, {'N', 14}, {'O', 15},
+                {'P', 16}, {'Q', 17}, {'R', 18}, {'S', 19}, {'T', 20},
+                {'U', 21}, {'V', 22}, {'W', 23}, {'X', 24}, {'Y', 25}, {'Z', 26},
+            };
+
         public bool TryAddLetterAssociations(string givenWord, string givenCode)
         {
             if (givenWord.Length == givenCode.Length)
